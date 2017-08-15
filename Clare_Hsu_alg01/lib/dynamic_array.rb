86de07a6a1dbca9ee87ad7030ecdd9a538eef0c7
Clare Hsu <clare.hsu390@gmail.com>
2017-08-15 14:32:57 -0700

    Edit dynamic array

--- Clare_Hsu_alg01/lib/dynamic_array.rb
+++ Clare_Hsu_alg01/lib/dynamic_array.rb
@@ -7,7 +7,6 @@ class DynamicArray
   def initialize(capacity = 8)
     @store = StaticArray.new(capacity)
     @length = 0
-    @start_idx = 0
     @capacity = capacity
   
   end
@@ -15,26 +14,36 @@ class DynamicArray
   # O(1)
   def [](index)
     check_index(index)
-    @store[(@start_idx + index) % capacity]
+    # @store[(@start_idx + index) % capacity]
+    @store[index]
   end
 
   # O(1)
   def []=(index, value)
     
-    check_index(index)
+    # if index >= @length
+    #   (index - @length).times { push(@store[index]) }
+    # elsif index < 0
+    #   # raise "index out of bounds" if index < -(@length)
+    #   return self[@length + index]
+    # end
+
     if index == @length
       resize! if capacity == @length
       @length += 1
     end
-    @store[(@start_idx + index) % capacity] = value
+    # @store[(@start_idx + index) % capacity] = value
+    @store[index] = value
   end
 
   # O(1)
   def pop
     raise "index out of bounds" if @length == 0
-    last_item = @store[(@start_idx + @length- 1) % capacity]
-    @length -= 1
-    last_item
+    # last_item = @store[(@start_idx + @length- 1) % capacity]
+   
+    last_item = self[self.length - 1]
+     @length -= 1
+   last_item
   end
 
   # def capacity
@@ -46,28 +55,39 @@ class DynamicArray
   def push(val)
   
     resize! if capacity == @length
-    @store[(@start_idx + @length) % capacity] = val
     @length += 1
+    self[self.length - 1] = val
     self
   end
 
   # O(n): has to shift over all the elements.
   def shift
     raise "index out of bounds" if @length == 0
+    val = self[0]
+    (1...length).each do |i|
+      self[i-1] = self[i]
+    end
     @length -= 1
-    first_item = @store[@start_idx]
-    @start_idx = (@start_idx + 1) % capacity
-    first_item
+    val
   end
 
   # O(n): has to shift over all the elements.
   def unshift(val)
     
     resize! if capacity == @length
-    @start_idx = (@start_idx - 1) % capacity
-    @store[@start_idx] = val
     @length += 1
+    i = 0
+    while i <= @length - 2
+      self[i+1] = self[i]
+      i += 1
+    end
+    # (@length - 2).downto(0).each do |i|
+    #   self[i+1] = self[i]
+    # end
+    self[0] = val
     self
+ 
+    
   end
 
   protected
@@ -75,14 +95,13 @@ class DynamicArray
   attr_writer :length
 
   def check_index(index)
-  
-    if index >= @length
+    # raise "index out of bounds" unless index.between?(0, length - 1)
+    if index >= @length || index < 0
       raise "index out of bounds"
-    elsif index < 0
-      raise "index out of bounds" if index < -(@length)
-      return self[@length + index]
+    # elsif index < 0
+    #   raise "index out of bounds" if index < -(@length)
+      # return self[@length + index]
     end
-    
   end
 
   # O(n): has to copy over all the elements to the new store.
