1a3e3b742f10912d97c9f783cea35059c660c1c2
Clare Hsu <clare.hsu390@gmail.com>
2017-08-17 16:27:45 -0700

    Implement first quicksort

---
+++ Clare_Hsu_alg04/lib/quick_sort.rb
@@ -0,0 +1,27 @@
+class QuickSort
+  # Quick sort has average case time complexity O(nlogn), but worst
+  # case O(n**2).
+
+  # Not in-place. Uses O(n) memory.
+  def self.sort1(array)
+    pivot = array.pop
+    right = []
+    left = []
+    array.each do |el|
+      if el >= pivot
+        right << pivot
+      else
+        left << pivot
+      end
+    end
+    sort1(left) + [pivot] + sort1(right)
+
+  end
+
+  # In-place.
+  def self.sort2!(array, start = 0, length = array.length, &prc)
+  end
+
+  def self.partition(array, start, length, &prc)
+  end
+end
