abc7db0829e57ca543df511180183f26634db379
Clare Hsu <clare.hsu390@gmail.com>
2017-08-18 06:31:04 -0700

    Implement quick sort 2"

--- Clare_Hsu_alg04/lib/quick_sort.rb
+++ Clare_Hsu_alg04/lib/quick_sort.rb
@@ -20,8 +20,34 @@ class QuickSort
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
+    prc ||= Proc.new {|x,y| x <=> y}
+
+    return array if length < 2
+    pivot = partition(array, start, length, &prc)
+    left_length = pivot - start
+    right_length = length - pivot - 1
+    sort2!(array, start, left_length, &prc)
+    sort2!(array, pivot + 1, right_length, &prc)
+
+    array
+
   end
 
   def self.partition(array, start, length, &prc)
+    prc ||= Proc.new {|x,y| x <=> y}
+
+    ((start + 1)...(start + length)).each do |idx|
+      val = array[idx]
+      if prc.call(array[start], val) == 1
+        array[idx] = array[start + 1]
+        array[start + 1] = array[start]
+        array[start] = val
+
+        start += 1
+      end
+    end
+
+    start
+
   end
 end
