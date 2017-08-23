cc9190b1fddf066a83c3b7c80595f5939c04aef9
Clare Hsu <clare.hsu390@gmail.com>
2017-08-23 11:07:11 -0700

    Add top down implementation

--- Clare_Hsu_alg07/lib/dynamic_programming.rb
+++ Clare_Hsu_alg07/lib/dynamic_programming.rb
@@ -38,29 +38,43 @@ class DynamicProgramming
   def frog_hops_top_down(n)
     return [[1]] if n == 1
     return [[1,1], [2]] if n == 2
+    frog_hops_top_down_helper(n)
+    
 
   end
 
   def frog_hops_top_down_helper(n)
+    @cache = {1 => [[1]], 2 => [[1,1], [2]], 3 => [[1,1,1], [1,2], [2,1], [3]]}
+    return @cache[n] if @cache[n]
+    (4..n).each do |i|
+      arr = []
+      (1..3).each do |num|
+        @cache[i - num].each do |el|
+          
+          arr << el + [num]
+        end
+      end
+        @cache[i] = arr
+    end
+
+    @cache[n]
 
   end
 
   def super_frog_hops(n, k)
+    
+   cache = {1 => [[1]], 2 => [[1,1], [2]], 3 => [[1,1,1], [1,2], [2,1], [3]]}
+    return cache[n].select {|el| el[0] <= k} if n > k && cache[n]
+    return cache[n] if cache[n]
+    
 
-    # arr = [[]]
-    # if k == 1
-    #   n.times do
-    #     arr[0] << 1
-    #   end
-    # end
-    # return arr
 
 
   end
 
   def knapsack(weights, values, capacity)
     return 0 if weights.empty? || values.empty? || capacity == 0
-    if values.last > capacity
+    if values.max > capacity
       return values.max
     end
 
