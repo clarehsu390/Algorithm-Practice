8e95086615ba4a778c6e8110f560e94a35a96df0
Clare Hsu <clare.hsu390@gmail.com>
2017-08-31 13:23:05 -0700

    Implement stackqueue

--- stack_queue/stackqueue.rb
+++ stack_queue/stackqueue.rb
@@ -2,13 +2,27 @@ require_relative 'stack.rb'
 
 class StackQueue
     def initialize
-        @in_stack = []
-        @out_stack = []
+        @in_stack = Stack.new
+        @out_stack = Stack.new
     end
 
-    def enqueue
+    def enqueue(el)
+        @in_stack.push(el)
     end
 
     def dequeue
+        #O(n), complete this once for every n dequeue --> ammortized to O(1)
+        until empty?
+            @out_stack << @in_stack.pop
+        end
+        @out_stack.pop
+    end
+
+    def empty?
+        @in_stack.length == 0
+    end
+
+    def size
+        @in_stack.length + @out_stack.length
     end
 end
